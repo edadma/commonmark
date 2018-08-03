575d694f560f56d2f2d9c215bebc0ed8d9046371
Edward A Maxedon <edadma@gmail.com>
2018-08-03 17:56:08 -0400

    cont coding phase 1

--- src/main/scala/CommonMarkParser.scala
+++ src/main/scala/CommonMarkParser.scala
@@ -5,32 +5,17 @@ import scala.collection.mutable.ArrayBuffer
 
 class CommonMarkParser {
 
-//  trait BlockType
-//  case object Break extends BlockType
-//  case object AHeading extends BlockType
-//  case object SHeading extends BlockType
-//  case object ICode extends BlockType
-//  case object FCode extends BlockType
-//  case object HTML extends BlockType
-//  case object Ref extends BlockType
-//  case object Par extends BlockType
-//  case object Blank extends BlockType
-//
-//  case object Doc extends BlockType
-//  case object Quote extends BlockType
-//  case object Lis extends BlockType
-//  case class Bullet( c: Char ) extends BlockType
-//  case class Ordered( c: Char ) extends BlockType
-//
-//  trait Block
-//  case class LeafBlock( var typ: BlockType, content: StringBuilder, handler: BlockHandler ) extends Block
-//  case class ContainerBlock( var typ: BlockType, content: ArrayBuffer[Block], handler: BlockHandler ) extends Block
-
   val blockTypes = new ArrayBuffer[BlockType]
 
   def parser( src: io.Source ) = {
 
     val doc = new DocumentBlock
+
+    def opens = {
+      def opens( b: Block ): Stream[Block] = Stream.cons( b, b.open.map(opens).getOrElse(Stream.empty) )
+
+      opens( doc )
+    }
 
     def next( s: Stream[String] ): Unit = {
       def matching( from: Int, b: Block ) = {
