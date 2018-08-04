bf9b5891b4096ea1031c0f8c34168acc02862f07
Edward A Maxedon <edadma@gmail.com>
2018-08-04 02:49:00 -0400

    phase 1

--- src/main/scala/CommonMarkParser.scala
+++ src/main/scala/CommonMarkParser.scala
@@ -10,23 +10,26 @@ class CommonMarkParser {
   def parser( src: io.Source ) = {
 
     val doc = new DocumentBlock
+    val trail = new ArrayBuffer[Block]
 
-    def opens = {
-      def opens( b: Block ): Stream[Block] = Stream.cons( b, b.open.map(opens).getOrElse(Stream.empty) )
+    trail += doc
 
-      opens( doc )
-    }
+//    def opens = {
+//      def opens( b: Block ): Stream[Block] = Stream.cons( b, b.open.map(opens).getOrElse(Stream.empty) )
+//
+//      opens( doc )
+//    }
 
     def next( s: Stream[String] ): Unit = {
-      def matching( from: Int, b: Block ) = {
-        b.accept( from, s ) match {
-          case None => b.open
-          case Some( newfrom ) =>
-            b.open match {
-              case None =>
-            }
-        }
-      }
+//      def matching( from: Int, b: Block ) = {
+//        b.accept( from, s ) match {
+//          case None => b.open
+//          case Some( newfrom ) =>
+//            b.open match {
+//              case None =>
+//            }
+//        }
+//      }
 
       if (s nonEmpty) {
         matching( 0, doc )
