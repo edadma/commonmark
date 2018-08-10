c432907b9da465e81464981fb5aeeb78a363023d
Edward A Maxedon <edadma@gmail.com>
2018-08-10 13:11:40 -0400

    cont coding lists

--- src/main/scala/ListBlock.scala
+++ src/main/scala/ListBlock.scala
@@ -3,28 +3,38 @@ package xyz.hyperreal.commonmark
 
 object ListBlockType extends BlockType {
 
-  val listRegex = """([-+*][ ]{1,4}|[0-9]{1,9}[.)])(.*)"""r
+  val bulletListRegex = """([-+*][ ]{1,4})(.*)"""r
+  val orderedListRegex = """([0-9]{1,9}[.)])(.*)"""r
+  val listRegex = """([ ]*)(.*)"""r
 
-  def accept( from: Int, text: String ) =
+  def accept( indent: Int, from: Int, text: String ) =
     text match {
-      case listRegex( marker, newtext ) => Some( (from + marker.length, newtext) )
+      case listRegex( marker, newtext ) if marker.length >= indent => Some( (from + marker.length, newtext) )
       case _ => None
     }
 
   def start(from: Int, text: String, s: Stream[String], prev: ContainerBlock, parser: CommonMarkParser, doc: DocumentBlock):
   Option[(Block, Int, String)] =
-    accept( from, text ) match {
-      case Some( (marker, newtext) ) => Some( (new QuoteBlock, from + marker, newtext) )
+    text match {
+      case bulletListRegex( marker, newtext ) =>
+        Some( (new ListBlock(BulletList(marker.head, marker.length)), from + marker.length, newtext) )
+      case orderedListRegex( marker, newtext ) =>
+        Some( (new ListBlock(OrderedList(marker dropRight 1 toInt, marker.length)), from + marker.length, newtext) )
       case _ => None
     }
 
 }
 
-class ListBlock extends ContainerBlock {
+abstract class ListType { val indent: Int }
+case class BulletList( marker: Char, indent: Int ) extends ListType
+case class OrderedList( start: Int, indent: Int ) extends ListType
+
+class ListBlock( typ: ListType ) extends ContainerBlock {
 
   val name = "list"
+  var loose = false
 
   def accept( from: Int, text: String, stream: Stream[String]) : Option[(Int, String)] =
-    QuoteBlockType.accept( from, text )
+    ListBlockType.accept( typ.indent, from, text )
 
 }
