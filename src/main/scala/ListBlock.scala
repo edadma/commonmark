26e6987dc4eea40e38f7fe7a3d36e41cde904130
Edward A Maxedon <edadma@gmail.com>
2018-08-13 06:49:07 -0400

    cont fixing lists

--- src/main/scala/ListBlock.scala
+++ src/main/scala/ListBlock.scala
@@ -3,26 +3,34 @@ package xyz.hyperreal.commonmark
 
 object ListBlockType extends BlockType {
 
-  val bulletListRegex = """[ ]{0,3}([-+*])[ ](.*)"""r
-  val orderedListRegex = """[ ]{0,3}([0-9]{1,9})([.)])[ ](.*)"""r
-  val listRegex = """  (.*)"""r
+  val bulletListRegex = """([ ]{0,3})([-+*])([ ]+)(.*)"""r
+  val orderedListRegex = """([ ]{0,3})([0-9]{1,9})([.)])([ ]+)(.*)"""r
+  val listRegex = """([ ]*)(.*)"""r
 
   def accept( list: ListBlock, from: Int, text: String ) =
     if (isBlank( text ))
       Some( (from, text) )
     else
       text match {
-        case listRegex( newtext ) => Some( (from + 2, newtext) )
+        case listRegex( spaces, newtext ) =>
+          if (spaces.length >= list.indent)
+            Some( (from + list.indent, " "*(spaces.length - list.indent) + newtext) )
+          else
+            None
         case _ => None
       }
 
   def start(from: Int, text: String, s: Stream[String], prev: ContainerBlock, parser: CommonMarkParser, doc: DocumentBlock):
   Option[(Block, Int, String)] =
     text match {
-      case bulletListRegex( marker, newtext ) =>
-        Some( (new ListBlock(BulletList(marker.head)), from + 2, newtext) )
-      case orderedListRegex( number, marker, newtext ) =>
-        Some( (new ListBlock(OrderedList(marker.head)) { typ.asInstanceOf[OrderedList].start = number.toInt }, from + 2, newtext) )
+      case bulletListRegex( indent, marker, spaces, newtext ) =>
+        val width = 1 + indent.length + spaces.length
+
+        Some( (new ListBlock(width, BulletList(marker.head)), from + width, newtext) )
+      case orderedListRegex( indent, number, marker, spaces, newtext ) =>
+        val width = 1 + number.length + indent.length + spaces.length
+
+        Some( (new ListBlock(width, OrderedList(marker.head)) { typ.asInstanceOf[OrderedList].start = number.toInt }, from + width, newtext) )
       case _ => None
     }
 
@@ -32,7 +40,7 @@ abstract class ListType
 case class BulletList( marker: Char ) extends ListType
 case class OrderedList( marker: Char ) extends ListType { var start = 1 }
 
-class ListBlock( val typ: ListType ) extends ContainerBlock {
+class ListBlock( val indent: Int, val typ: ListType ) extends ContainerBlock {
 
   val name = "list"
   var tight = true
--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -6,12 +6,16 @@ object Main extends App {
   val p = new CommonMarkParser
   val input =
     """
-      |10) foo
-      |   - bar
+      |1.  A paragraph
+      |    with two lines.
+      |
+      |        indented code
+      |
+      |    > A block quote.
     """.trim.stripMargin
 
   val doc = p.parse( input )
 
-  println( p.parseBlocks( input.lines.toStream ))
+//  println( p.parseBlocks( input.lines.toStream ))
   println( Util.html(doc, 2) )
 }
