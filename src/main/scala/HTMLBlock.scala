02cff8ab83f5a40eba3b329b6f914fecbb3b3794
Edward A Maxedon <edadma@gmail.com>
2018-08-13 21:59:08 -0400

    cont coding html block

--- src/main/scala/HTMLBlock.scala
+++ src/main/scala/HTMLBlock.scala
@@ -6,15 +6,23 @@ object HTMLBlockType extends BlockType {
 
   val start1Regex = """(?i)(?:<script|<pre|<style)\s*>?"""r
   val end1Regex = """.*(?:</script>|</pre>|</style>).*"""r
+  val start4Regex = """<![A-Z]"""r
+  val start5Regex = """(?i)</?(?:address|article|aside|base|basefont|blockquote|body|caption|center|col|colgroup|dd|details|dialog|dir|div|dl|dt|fieldset|figcaption|figure|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hr|html|iframe|legend|li|link|main|menu|menuitem|meta|nav|noframes|ol|optgroup|option|p|param|section|source|summary|table|tbody|td|tfoot|th|thead|title|tr|track|ul)\s*(?:/?>)?"""r
   val starts =
     List(
       (from: Int, text: String, s: Stream[String]) => start1Regex.pattern.matcher( text ).matches,
-      (from: Int, text: String, s: Stream[String]) => text startsWith "<!--"
+      (from: Int, text: String, s: Stream[String]) => text startsWith "<!--",
+      (from: Int, text: String, s: Stream[String]) => text startsWith "<?",
+      (from: Int, text: String, s: Stream[String]) => start4Regex.pattern.matcher( text ).matches,
+      (from: Int, text: String, s: Stream[String]) => start5Regex.pattern.matcher( text ).matches
     )
   val ends =
     Vector(
       (from: Int, text: String, s: Stream[String]) => HTMLBlockType.end1Regex.pattern.matcher( text ).matches,
-      (from: Int, text: String, s: Stream[String]) => text contains "-->"
+      (from: Int, text: String, s: Stream[String]) => text contains "-->",
+      (from: Int, text: String, s: Stream[String]) => text contains "?>",
+      (from: Int, text: String, s: Stream[String]) => text contains ">",
+      (from: Int, text: String, s: Stream[String]) => s.isEmpty || isBlank( s.tail.head substring from )
     )
 
   override def start(from: Int, text: String, s: Stream[String], prev: ContainerBlock, parser: CommonMarkParser, doc: DocumentBlock): Option[(Block, Int, String)] = {
